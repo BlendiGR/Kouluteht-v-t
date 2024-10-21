425a57b39feeac62211addf68415229f000d7837
BlendinAMKura <blendi.gr@hotmail.fi>
2024-10-21 11:15:12 +0300

    Tehtävät

--- Python Tehtävät/Py-Olio-Tehtävä9.py
+++ Python Tehtävät/Py-Olio-Tehtävä9.py
@@ -1,3 +1,6 @@
+import random
+
+
 #KOULUTEHTÄVÄT 
 
 #-----Luokka, olio, alustaja-------#
@@ -48,7 +51,34 @@ Mercedes.kulje(10)
 print(f"Mercedes kulkee tällä hetkellä {Mercedes.tämänhetkinen_nopeus}km/h ja matkaa on kulunut {Mercedes.kuljettu_matka}km")
 
 #----- TEHTÄVÄ -  4 -----#
-
 auto1 = Auto("ABC-1", 140)
 auto2 = Auto("ABC-2", 200)
-auto2 = Auto("ABC-3", 150)
+auto3 = Auto("ABC-3", 150)
+auto4 = Auto("ABC-4", 170)
+auto5 = Auto("ABC-5", 190)
+auto6 = Auto("ABC-6", 100)
+auto7 = Auto("ABC-7", 130)
+auto8 = Auto("ABC-8", 180)
+auto9 = Auto("ABC-9", 110)
+auto10 = Auto("ABC-10", 160)
+
+kisa_autot = [auto1, auto2, auto3, auto4, auto5, auto6, auto7, auto8, auto9, auto10]
+
+kilpailu = True
+
+while kilpailu:
+     for kisa_auto in kisa_autot:
+        nopeus_muutos = random.randint(-15, 15)
+        kisa_auto.kiihdytä(nopeus_muutos)
+
+        kisa_auto.kulje(1)
+
+        if kisa_auto.kuljettu_matka >= 10000:
+            kilpailu = False
+            break
+    
+print("Kisa loppu")
+
+for kisa_auto in kisa_autot:
+    print(f"Auton rekkari {kisa_auto.rekisteritunnus}, kuljettu matka {kisa_auto.kuljettumatka} km!")
+
