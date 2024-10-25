af76ad41c393528cb94afb9a80fcd664f6b9204a
BlendinAMKura <blendi.gr@hotmail.fi>
2024-10-25 10:59:26 +0300

    Matematiikka Tehtävät

---
+++ Python Tehtävät/Matematiikka Python Tehtävät.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+####################################################################### MATEMATIIKKA 2, OSA 1 ########################################################################
+
+#Tehtävä 1 1.1, s8-9
+
+#1. 
+
+#a) & b)
+
+t1 = [2.493, 0.911]
+
+for rad in t1:
+    print(f"Tehtävä 1 Vastaus : {np.degrees(rad)}")
+
+#1.
+
+#a) & b)
+
+t2 = [137.7, 62.3]
+for deg in t2:
+    print(f" Tehtävä 2 Vastaus : {np.radians(deg)}")
+
+#3.
+
+t3 = [30, 45, 60, 90, 120, 135, 150, 180, 270, 360]
+
+for deg in t3:
+    print(f"Tehtävä 3. Vastaus : {np.radians(deg)} ")
+
